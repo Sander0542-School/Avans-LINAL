7584c64cb40b17400749699a617770cf8f3b36c5
Tommy Hosewol <t.hosewol@gmail.com>
2021-12-08 16:54:54 +0100

    Moved to game

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,9 @@
-#include "Engine/InputManager.hpp"
-#include "Engine/Window.hpp"
-#include "Models/Models.hpp"
-
-#include "Entities/Entities.hpp"
-
-#include <vector>
-
-using namespace linal::engine;
-using namespace linal::models;
-using namespace linal::entities;
-using namespace linal::enums;
-
-const int WINDOW_WIDTH = 800;
-const int WINDOW_HEIGHT = 450;
-
-const int SCALE = 25;
+#include "Game.hpp"
 
 int main(int argc, char* args[])
 {
-    std::vector<Window> windows;
-    windows.emplace_back("LINAL Assessment - Front", 10, 50, WINDOW_WIDTH, WINDOW_HEIGHT, WindowView::Front);
-    windows.emplace_back("LINAL Assessment - Top", 10, WINDOW_HEIGHT + 100, WINDOW_WIDTH, WINDOW_HEIGHT, WindowView::Top);
-    windows.emplace_back("LINAL Assessment - Side", WINDOW_WIDTH + 20, WINDOW_HEIGHT + 100, WINDOW_WIDTH, WINDOW_HEIGHT, WindowView::Side);
-
-    Point worldCenter{WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0};
-    int worldScale = SCALE;
-
-    std::vector<std::shared_ptr<linal::entities::common::IDrawable>> drawables;
-    auto block = std::make_shared<Block>();
-    drawables.push_back(block);
-
-    while (true)
-    {
-        if (!InputManager::GetInstance().Update()) break;
-
-        for (auto& window: windows) window.Clear();
-
-        // transform models
-
-        for (const auto& drawable: drawables)
-        {
-            for (auto& window: windows)
-                drawable->Draw(window, worldCenter, worldScale);
-        }
-
-        for (auto& window: windows) window.SwapBuffers();
-
-        SDL_Delay(25);
-    }
+    linal::Game game;
+    game.Start();
 
     return 0;
 }
