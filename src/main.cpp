49e069ed55d9c6c20292a99ce83f408141a1cd69
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-08 12:52:49 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,28 @@
 
 #include "Entities/Entities.hpp"
 
+#include <vector>
+
 using namespace linal::engine;
 using namespace linal::models;
+using namespace linal::entities;
 
 const int WINDOW_WIDTH = 1200;
 const int WINDOW_HEIGHT = 600;
 
+const int SCALE = 30;
+
 int main(int argc, char* args[])
 {
+    std::vector<std::shared_ptr<linal::entities::common::IDrawable>> drawables;
+
     Window window{"LINAL Assessment", 50, 50, WINDOW_WIDTH, WINDOW_HEIGHT};
 
-//    Point worldCenter{WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0};
+    Point worldCenter{WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 0};
+    int worldScale = SCALE;
+
+    auto block = std::make_shared<Block>();
+    drawables.push_back(block);
 
     while (true)
     {
@@ -24,7 +35,10 @@ int main(int argc, char* args[])
 
         // transform models
 
-        // draw models
+        for (const auto& drawable: drawables)
+        {
+            drawable->Draw(window, worldCenter, worldScale);
+        }
 
         window.SwapBuffers();
 
