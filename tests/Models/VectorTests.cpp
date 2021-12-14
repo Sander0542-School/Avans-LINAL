8eb6179db7d622817fb7ff5d7fd037a48c4053a2
Sander Jochems <sanderjochems@hotmail.nl>
2021-12-14 13:17:39 +0100

    Update VectorTests.cpp

--- tests/Models/VectorTests.cpp
+++ tests/Models/VectorTests.cpp
@@ -1,12 +1,25 @@
 #include <gtest/gtest.h>
 #include "Models/Vector.hpp"
 
+#include <cmath>
+
 using namespace linal::models;
 
-TEST(VectorTests, DotProductMethod)
+TEST(VectorTests, TestDotProductMethod)
 {
     Vector vectorV{2, 4, 0};
     Vector vectorW{4, 1, 0};
 
-    ASSERT_TRUE(vectorV.DotProduct(vectorW) == 12);
+    ASSERT_EQ(vectorV.DotProduct(vectorW), 12);
+}
+
+TEST(VectorTests, TestAngleMethod)
+{
+    Vector vectorV{2, 4, 0};
+    Vector vectorW{3, 2, 0};
+
+    auto angle = vectorV.Angle(vectorW);
+    angle = std::ceil(angle * 100) / 100;
+
+    ASSERT_EQ(angle, 0.87);
 }
