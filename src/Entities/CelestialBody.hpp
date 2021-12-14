9c9f7537b9c2ba59475e97dc7e11a18351298066
Tommy Hosewol <t.hosewol@gmail.com>
2021-12-14 15:26:11 +0100

    Update CelestialBody.hpp

--- src/Entities/CelestialBody.hpp
+++ src/Entities/CelestialBody.hpp
@@ -8,46 +8,42 @@ namespace linal::entities
     class CelestialBody : public common::Entity
     {
         private:
-            models::Matrix _rotateLeftMatrix = models::Matrix::Roll(2.5);
-            models::Matrix _rotateRightMatrix = models::Matrix::Roll(-2.5);
+            models::Matrix _rollLeftMatrix = models::Matrix::Roll(2.5);
+            models::Matrix _rollRightMatrix = models::Matrix::Roll(-2.5);
+            models::Matrix _yawLeftMatrix = models::Matrix::Yaw(2.5);
+            models::Matrix _yawRightMatrix = models::Matrix::Yaw(-2.5);
+            models::Matrix _pitchLeftMatrix = models::Matrix::Pitch(2.5);
+            models::Matrix _pitchRightMatrix = models::Matrix::Pitch(-2.5);
             models::Matrix _scaleUpMatrix = models::Matrix::Scaling(1.05, 1.05, 1.05);
             models::Matrix _scaleDownMatrix = models::Matrix::Scaling(0.95, 0.95, 0.95);
 
         public:
             CelestialBody()
             {
-                // POINTS
-                {
-                    _points.emplace_back(0, 1, -1); // 0
-                    _points.emplace_back(1, 0, -1); // 1
-                    _points.emplace_back(0, -1, -1); // 2
-                    _points.emplace_back(-1, 0, -1); // 3
+                _points.emplace_back(-3, -3, -3);
+                _points.emplace_back(3, -3, -3);
+                _points.emplace_back(3, 3, -3);
+                _points.emplace_back(-3, 3, -3);
+                _points.emplace_back(-3, -3, 3);
+                _points.emplace_back(3, -3, 3);
+                _points.emplace_back(3, 3, 3);
+                _points.emplace_back(-3, 3, 3);
 
-                    _points.emplace_back(0, 1, 1); // 4
-                    _points.emplace_back(1, 0, 1); // 5
-                    _points.emplace_back(0, -1, 1); // 6
-                    _points.emplace_back(-1, 0, 1); // 7
-                }
+                _lines.emplace_back(0, 1);
+                _lines.emplace_back(1, 2);
+                _lines.emplace_back(2, 3);
+                _lines.emplace_back(3, 0);
 
-                // LINES
-                {
-                    _lines.emplace_back(0, 1);
-                    _lines.emplace_back(1, 2);
-                    _lines.emplace_back(2, 3);
-                    _lines.emplace_back(3, 0);
+                _lines.emplace_back(4, 5);
+                _lines.emplace_back(5, 6);
+                _lines.emplace_back(6, 7);
+                _lines.emplace_back(7, 4);
 
-                    _lines.emplace_back(4, 5);
-                    _lines.emplace_back(5, 6);
-                    _lines.emplace_back(6, 7);
-                    _lines.emplace_back(7, 4);
+                _lines.emplace_back(0, 4);
+                _lines.emplace_back(1, 5);
+                _lines.emplace_back(2, 6);
+                _lines.emplace_back(3, 7);
 
-                    _lines.emplace_back(0, 4);
-                    _lines.emplace_back(1, 5);
-                    _lines.emplace_back(2, 6);
-                    _lines.emplace_back(3, 7);
-                }
-
-                // CENTER
                 _center = {0, 0, 0};
             }
 
@@ -55,16 +51,51 @@ namespace linal::entities
             {
                 if (!engine::Input::AnyKey()) return;
 
-                // ROTATION
+                // ROTATION: ROLL
                 if (engine::Input::GetKey(engine::Input::KeyCode::C))
                 {
                     auto center = this->Center();
-                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _rotateLeftMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _rollLeftMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
                 }
                 if (engine::Input::GetKey(engine::Input::KeyCode::V))
                 {
                     auto center = this->Center();
-                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _rotateRightMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _rollRightMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                }
+                // ROTATION: ROLL
+                if (engine::Input::GetKey(engine::Input::KeyCode::C))
+                {
+                    auto center = this->Center();
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _rollLeftMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                }
+                if (engine::Input::GetKey(engine::Input::KeyCode::V))
+                {
+                    auto center = this->Center();
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _rollRightMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                }
+
+                // ROTATION: YAW
+                if (engine::Input::GetKey(engine::Input::KeyCode::B))
+                {
+                    auto center = this->Center();
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _yawLeftMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                }
+                if (engine::Input::GetKey(engine::Input::KeyCode::N))
+                {
+                    auto center = this->Center();
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _yawRightMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                }
+
+                // ROTATION: PITCH
+                if (engine::Input::GetKey(engine::Input::KeyCode::O))
+                {
+                    auto center = this->Center();
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _pitchLeftMatrix * models::Matrix::Translation(-center.x, -center.y, -center.z));
+                }
+                if (engine::Input::GetKey(engine::Input::KeyCode::P))
+                {
+                    auto center = this->Center();
+                    this->Transform(models::Matrix::Translation(center.x, center.y, center.z) * _pitchRightMatrix  * models::Matrix::Translation(-center.x, -center.y, -center.z));
                 }
 
                 // SCALING
